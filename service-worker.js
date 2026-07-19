14d9b75c3090341959c5baf4654e133942f3815b
lingarama216-dot <lingarama216@gmail.com>
2026-07-20 00:19:59 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "sukun-pad-v1";
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
